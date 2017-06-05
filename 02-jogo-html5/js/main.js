05bbc05a6a49585382d9ce9264ab2a2a8e03e143
P. Dayvson <pdayvson@hotmail.com>
2017-06-04 21:31:57 -0300

    Jogo HTML5 inital commit

---
+++ 02-jogo-html5/js/main.js
@@ -0,0 +1,39 @@
+var canvas, context, altura, largura, frames = 0;
+
+function clique(event) {
+	console.log("clicou");
+}
+
+function main() {
+	altura = window.innerHeight;
+	largura = window.innerWidth;
+
+	if (largura >= 500) {
+		largura = 600;
+		altura = 600;
+	}
+
+	canvas = document.createElement("canvas");
+	canvas.width = largura;
+	canvas.height = altura;
+	canvas.style.border = "1px solid #000";
+
+	context = canvas.getContext("2d");
+	document.body.appendChild(canvas);
+	document.addEventListener("mousedown", clique);
+}
+
+function roda() {
+	// body...
+}
+
+function atualiza() {
+	// body...
+}
+
+function desenha() {
+	// body...
+}
+
+// inicializa o jogo
+main();
