ad05a83dd55c1f0be004132134504864a4dab4ee
Paulo Dayvson <pdayvson@hotmail.com>
2017-07-24 16:43:10 -0300

    Nem lembro o que eu mudei

--- 02-jogo-html5/js/main.js
+++ 02-jogo-html5/js/main.js
@@ -1,38 +1,39 @@
 var canvas, context, altura, largura, frames = 0;
 
 function clique(event) {
-	console.log("clicou");
+    alert('clicou');
+    console.log('teste');
 }
 
 function main() {
-	altura = window.innerHeight;
-	largura = window.innerWidth;
+    altura = window.innerHeight;
+    largura = window.innerWidth;
 
-	if (largura >= 500) {
-		largura = 600;
-		altura = 600;
-	}
+    if (largura >= 500) {
+        largura = 600;
+        altura = 600;
+    }
 
-	canvas = document.createElement("canvas");
-	canvas.width = largura;
-	canvas.height = altura;
-	canvas.style.border = "1px solid #000";
+    canvas = document.createElement('canvas');
+    canvas.width = largura;
+    canvas.height = altura;
+    canvas.style.border = '1px solid #000';
 
-	context = canvas.getContext("2d");
-	document.body.appendChild(canvas);
-	document.addEventListener("mousedown", clique);
+    context = canvas.getContext('2d');
+    document.body.appendChild(canvas);
+    document.addEventListener('mousedown', clique);
 }
 
 function roda() {
-	// body...
+    // body...
 }
 
 function atualiza() {
-	// body...
+    // body...
 }
 
 function desenha() {
-	// body...
+    // body...
 }
 
 // inicializa o jogo
